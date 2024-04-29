05b1317a7504444d3664d24b0062a81e1c64557e
tsunghan <tsunghan8814@gmail.com>
2024-04-29 10:41:44 +0800

    新增test，找消失點

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-X = np.arange(1, 3, 1)
-Y = np.arange(0, 2, 1)
+X = np.arange(1, 6, 1)
+Y = np.arange(6, 1, -1)
 u, v = np.meshgrid(X, Y)
 
 magnitude = np.sqrt(u**2 + v**2)
@@ -12,7 +12,7 @@ angle = np.arctan2(u, v)
 # 將角度轉換為 0 到 360 的範圍
 angl_degree = (angle* 180 / np.pi)
 
-polar = np.zeros((len(Y), len(X), 2))
+polar = np.zeros((len(u), len(v), 2))
 
 # 儲存 magnitude 和 angl_degree 到 polar 中
 polar[:,:,0] = magnitude
@@ -23,8 +23,8 @@ u = u - 128
 v = v.astype("int16")
 v = v - 128
 
-
-plt.quiver(u, v, color="red", scale=0.01)  # scale 控制箭頭的大小
+print(polar)
+plt.quiver(u, v, color="red")  # scale 控制箭頭的大小
 plt.title('Combined Displacement Vector Field')
 plt.xlabel('X-axis')
 plt.ylabel('Y-axis')
@@ -32,8 +32,8 @@ plt.show()
 
 
 
-C = np.sin(U)
+# C = np.sin(U)
 
-fig, ax = plt.subplots()
-q = ax.quiver(X, Y, U, V, C)
-plt.show()
+# fig, ax = plt.subplots()
+# q = ax.quiver(X, Y, U, V, C)
+# plt.show()
